da464fafe31ac753eeb0682321c4784913a0c066
mjgjtie <thienvq97@gmail.com>
2019-04-26 08:30:38 +0700

    dsad

---
+++ src/app/_nav.ts
@@ -0,0 +1,178 @@
+export const navItemsAdmin = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  
+  {
+    title: true,
+    name: 'Accounts'
+  },
+  // {
+  //   name: 'Add New User',
+  //   url: '/base',
+  //   icon: 'icon-puzzle',
+  //   children: [
+  //     {
+  //       name: 'Cards',
+  //       url: '/base/cards',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Carousels',
+  //       url: '/base/carousels',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Collapses',
+  //       url: '/base/collapses',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Forms',
+  //       url: '/base/forms',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Pagination',
+  //       url: '/base/paginations',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Popovers',
+  //       url: '/base/popovers',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Progress',
+  //       url: '/base/progress',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Switches',
+  //       url: '/base/switches',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Tables',
+  //       url: '/base/tables',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Tabs',
+  //       url: '/base/tabs',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Tooltips',
+  //       url: '/base/tooltips',
+  //       icon: 'icon-puzzle'
+  //     }
+  //   ]
+  // },
+  {
+    name: 'Add New Users',
+    url: '/buttons/',
+    icon: 'icon-user-follow'
+  },
+  {
+    name: 'Edit Users',
+    url: '/base/tables',
+    icon: 'icon-cursor'
+  }
+];
+
+export const navItemsStaff = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  
+  {
+    title: true,
+    name: 'Staff'
+  },
+  // {
+  //   name: 'Add New User',
+  //   url: '/base',
+  //   icon: 'icon-puzzle',
+  //   children: [
+  //     {
+  //       name: 'Cards',
+  //       url: '/base/cards',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Carousels',
+  //       url: '/base/carousels',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Collapses',
+  //       url: '/base/collapses',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Forms',
+  //       url: '/base/forms',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Pagination',
+  //       url: '/base/paginations',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Popovers',
+  //       url: '/base/popovers',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Progress',
+  //       url: '/base/progress',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Switches',
+  //       url: '/base/switches',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Tables',
+  //       url: '/base/tables',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Tabs',
+  //       url: '/base/tabs',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Tooltips',
+  //       url: '/base/tooltips',
+  //       icon: 'icon-puzzle'
+  //     }
+  //   ]
+  // },
+  
+  {
+    name: 'Document',
+    url: '/base/tables',
+    icon: 'icon-dashboard'
+  },
+  {
+    name: 'Report',
+    url: '/base/tables',
+    icon: 'icon-note'
+  }
+];
